5c4ea8e72dcbae09e716d949c83650d031621fa3
PROFESSIONAL <nairs77@gmail.com>
2018-12-19 01:30:56 +0900

    Inital Commit

---
+++ app/src/main/java/no/matter/who/mediaplayer/MainActivity.kt
@@ -0,0 +1,119 @@
+package who.matter.no.mediaplayer
+
+import android.graphics.Point
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Surface
+import android.view.SurfaceHolder
+import android.view.View
+import android.widget.FrameLayout
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import java.lang.IllegalArgumentException
+import java.lang.IllegalStateException
+import java.util.jar.Attributes
+
+class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
+
+    private lateinit var mediaPlayer: MediaPlayer
+    private var playbackPosition = 0
+    //private val rtspUrl = "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov"
+    private val rtspUrl = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val holder = surfaceView.holder
+        holder.addCallback(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        playbackPosition = mediaPlayer.currentPosition
+    }
+
+    override fun onStop() {
+        mediaPlayer.stop()
+        mediaPlayer.release()
+
+        super.onStop()
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder?) {
+
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder?) {
+        val surface = holder?.surface
+
+        if (surface != null)
+            setupMediaPlayer(surface)
+
+        prepareMediaPlayer()
+    }
+
+    private fun createAudioAttributes(): AudioAttributes {
+        val builder = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_MOVIE)
+        return builder.build()
+    }
+
+    private fun setupMediaPlayer(surface: Surface) {
+        progressBar.visibility = View.VISIBLE
+        mediaPlayer = MediaPlayer()
+        mediaPlayer.setSurface(surface)
+
+        val audioAttributes = createAudioAttributes()
+        mediaPlayer.setAudioAttributes(audioAttributes)
+
+        val uri = Uri.parse(rtspUrl)
+        try {
+            mediaPlayer.setDataSource(this, uri)
+        } catch (e: IllegalArgumentException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    private fun setSurfaceDimensions(player: MediaPlayer, width: Int, height: Int) {
+        if (width > 0 && height > 0) {
+            val aspectRatio = height.toFloat() / width.toFloat()
+            val screenDimensions = Point()
+            windowManager.defaultDisplay.getSize(screenDimensions)
+            val surfaceWidth = screenDimensions.x
+            val surfaceHeight = (surfaceWidth * aspectRatio).toInt()
+            val params = FrameLayout.LayoutParams(surfaceWidth, surfaceHeight)
+            surfaceView.layoutParams = params
+
+            val holder = surfaceView.holder
+            player.setDisplay(holder)
+        }
+    }
+    private fun prepareMediaPlayer() {
+        try {
+            mediaPlayer.prepareAsync()
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        }
+
+        mediaPlayer.setOnPreparedListener {
+            progressBar.visibility = View.INVISIBLE
+            mediaPlayer.seekTo(playbackPosition)
+            mediaPlayer.start()
+        }
+
+        mediaPlayer.setOnVideoSizeChangedListener { player, width, height ->
+            setSurfaceDimensions(player, width, height)
+        }
+    }
+}
---
+++ mediaitem/src/main/java/no/matter/who/mediaitem/MediaItem.kt
@@ -0,0 +1,5 @@
+package no.matter.who.mediaitem
+
+class MediaItem {
+
+}
